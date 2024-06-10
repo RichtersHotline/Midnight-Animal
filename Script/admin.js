8fd6cf9d7bae202bd600faa20b2883f2ca787693
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-10 11:45:14 +0200

    added company name copyright and current year to footer

--- Script/admin.js
+++ Script/admin.js
@@ -1,1 +1,8 @@
 // Javascript 
+
+
+let Copyright = document.getElementById("Foot3r")
+ 
+let currentyear = new Date
+
+Copyright.innerHTML = "Midnight Animal"  + "   " + "Copyright" + "      " + "&#169;" +  "        " + currentyear.getUTCFullYear()
